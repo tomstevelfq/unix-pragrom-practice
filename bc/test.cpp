474c9ed06a87a0e9e290eb2ecb381a7a458d460c
lufuqiang <tomstevelfq@gmail.com>
2023-07-01 14:40:07 +0800

    .

---
+++ bc/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    const char buff[256]="hello this";
+    cout<<buff<<endl;
+    cout<<strlen(buff)<<endl;
+    return 0;
+}
